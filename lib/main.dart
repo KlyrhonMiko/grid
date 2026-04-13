6b85aa987e606cae6b92154be03e5d952cce57e6
Klyrhon <139136307+KlyrhonMiko@users.noreply.github.com>
2026-04-13 19:07:39 +0800

    feat: instagram oauth

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'providers/instagram_provider.dart';
 import 'screens/connect_screen.dart';
 import 'screens/profile_screen.dart';
 import 'theme.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
